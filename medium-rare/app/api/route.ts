0812c73dfbc21d3141480b22ff628c20a6765c1b
justice-rest <bonjour@justice.rest>
2025-03-16 19:33:14 +0530

    Jesus W!

---
+++ medium-rare/app/api/route.ts
@@ -0,0 +1,148 @@
+import Groq from "groq-sdk";
+import { headers } from "next/headers";
+import { z } from "zod";
+import { zfd } from "zod-form-data";
+import { unstable_after as after } from "next/server";
+
+const groq = new Groq();
+
+const schema = zfd.formData({
+	input: z.union([zfd.text(), zfd.file()]),
+	message: zfd.repeatableOfType(
+		zfd.json(
+			z.object({
+				role: z.enum(["user", "assistant"]),
+				content: z.string(),
+			})
+		)
+	),
+});
+
+export async function POST(request: Request) {
+	console.time("transcribe " + request.headers.get("x-vercel-id") || "local");
+
+	const { data, success } = schema.safeParse(await request.formData());
+	if (!success) return new Response("Invalid request", { status: 400 });
+
+	const transcript = await getTranscript(data.input);
+	if (!transcript) return new Response("Invalid audio", { status: 400 });
+
+	console.timeEnd(
+		"transcribe " + request.headers.get("x-vercel-id") || "local"
+	);
+	console.time(
+		"text completion " + request.headers.get("x-vercel-id") || "local"
+	);
+
+	const completion = await groq.chat.completions.create({
+		model: "llama3-8b-8192",
+		messages: [
+			{
+				role: "system",
+				content: `- You are Robert, a not-so friendly and definetly rude assistent
+			- You believe thick of it is a top-tier song
+			- Your opinions should be starkly, weirdly, and humrously differently (Dark humor but PG-13)
+			- Your goal is to roast tthe user based on their love life, which you can ask the user to tell you about.
+			- If you don't understand the user's request, ask for clarification.
+			- You do not have access to up-to-date information, so you should not provide real-time data.
+			- You are not capable of performing actions other than responding to the user.
+			- Do not use markdown, emojis, or other formatting in your responses. Respond in a way easily spoken by text-to-speech software.
+			- User location is ${location()}.
+			- The current time is ${time()}.
+			- Your built by Team BurnedByHer`,
+			},
+			...data.message,
+			{
+				role: "user",
+				content: transcript,
+			},
+		],
+	});
+
+	const response = completion.choices[0].message.content;
+	console.timeEnd(
+		"text completion " + request.headers.get("x-vercel-id") || "local"
+	);
+
+	console.time(
+		"cartesia request " + request.headers.get("x-vercel-id") || "local"
+	);
+
+	const voice = await fetch("https://api.cartesia.ai/tts/bytes", {
+		method: "POST",
+		headers: {
+			"Cartesia-Version": "2024-06-30",
+			"Content-Type": "application/json",
+			"X-API-Key": process.env.CARTESIA_API_KEY!,
+		},
+		body: JSON.stringify({
+			model_id: "sonic-english",
+			transcript: response,
+			voice: {
+				mode: "id",
+				id: "79a125e8-cd45-4c13-8a67-188112f4dd22",
+			},
+			output_format: {
+				container: "raw",
+				encoding: "pcm_f32le",
+				sample_rate: 24000,
+			},
+		}),
+	});
+
+	console.timeEnd(
+		"cartesia request " + request.headers.get("x-vercel-id") || "local"
+	);
+
+	if (!voice.ok) {
+		console.error(await voice.text());
+		return new Response("Voice synthesis failed", { status: 500 });
+	}
+
+	console.time("stream " + request.headers.get("x-vercel-id") || "local");
+	after(() => {
+		console.timeEnd(
+			"stream " + request.headers.get("x-vercel-id") || "local"
+		);
+	});
+
+	return new Response(voice.body, {
+		headers: {
+			"X-Transcript": encodeURIComponent(transcript),
+			"X-Response": encodeURIComponent(response),
+		},
+	});
+}
+
+function location() {
+	const headersList = headers();
+
+	const country = headersList.get("x-vercel-ip-country");
+	const region = headersList.get("x-vercel-ip-country-region");
+	const city = headersList.get("x-vercel-ip-city");
+
+	if (!country || !region || !city) return "unknown";
+
+	return `${city}, ${region}, ${country}`;
+}
+
+function time() {
+	return new Date().toLocaleString("en-US", {
+		timeZone: headers().get("x-vercel-ip-timezone") || undefined,
+	});
+}
+
+async function getTranscript(input: string | File) {
+	if (typeof input === "string") return input;
+
+	try {
+		const { text } = await groq.audio.transcriptions.create({
+			file: input,
+			model: "whisper-large-v3",
+		});
+
+		return text.trim() || null;
+	} catch {
+		return null; // Empty audio file
+	}
+}
